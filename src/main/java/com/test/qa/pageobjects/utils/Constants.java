f33b6c4f4a3b98e39f9d840abeb2e9371eb56cb1
JeyapriyaJegatheeswaran <jjypriya@gmail.com>
2019-07-30 09:55:06 +0530

    for batch 2

---
+++ src/main/java/com/test/qa/pageobjects/utils/Constants.java
@@ -0,0 +1,15 @@
+package com.test.qa.pageobjects.utils;
+
+public class Constants {
+	public static final String UBUNTU = "ubuntu";
+	public static final String WINDOWS = "windows";
+
+	public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
+
+
+	public static final String LOGIN_LINK = "/login";
+
+
+
+}	
