836e0148ed7edb4835c19e843db814c58f3d4be6
Piratheepan <48407945+Piratheepan@users.noreply.github.com>
2019-08-05 14:55:49 +0530

    Update PageBase.java

--- src/main/java/com/test/qa/pageobjects/utils/PageBase.java
+++ src/main/java/com/test/qa/pageobjects/utils/PageBase.java
@@ -22,7 +22,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class PageBase {
 	private static WebDriver driver;
 
-    private static String baseUrl = "https://sgic-team2.github.io/defecttracker";
+    private static String baseUrl = "http://localhost:3000";
 	private static String webDriverLocation = "src"+File.separator+"test"+File.separator+"resources"+File.separator+"drivers"+File.separator;
     protected static String downloadFilepath = System.getProperty("user.dir")+File.separator+"src"+File.separator+"test"+File.separator+"resources"+File.separator+"fileDownload";
     protected static String uploadFilepath = System.getProperty("user.dir")+File.separator+"src"+File.separator+"test"+File.separator+"resources"+File.separator+"fileUpload";
