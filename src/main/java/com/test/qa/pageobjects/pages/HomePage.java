159452334ffa42dbd5345bc83a5c0afa557d2e4d
JeyapriyaJegatheeswaran <jjypriya@gmail.com>
2019-08-05 10:46:05 +0530

    test

--- src/main/java/com/test/qa/pageobjects/pages/HomePage.java
+++ src/main/java/com/test/qa/pageobjects/pages/HomePage.java
@@ -13,31 +13,23 @@ public class HomePage extends PageBase {
 
 	private static final Logger LOGGER = Logger.getLogger(HomePage.class);
 
-	private static By hdrWelcome = By.className("heading");
-	private static String commonLinkString = "//a[@href='LINK']";
+	private static By hdrWelcome = By.xpath("//div[@class = 'logo']");
 
-	/**
-	 * Verify Home Page Displayed
-	 *
-	 * @throws null
-	 * @return boolean
-	 */
+
+	private static String commonLinkString = "//a[@href='LINK']";
+	private static String commonLink = "//div[@class='THANU']";
 	public static boolean isHomePageDisplayed() {
 		staticWait(4);
 		return getDriver().findElement(hdrWelcome).isDisplayed();
-
 	}
-
-	/**
-	 * Click Link
-	 *
-	 * @throws null
-	 * @param link
-	 * @return void
-	 */
 	public static void clickLink(String link) {
 		getDriver().findElement(By.xpath(commonLinkString.replace("LINK", link))).click();
 		LOGGER.info("Link " + link + " Clicked");
+		staticWait(3);
+	}
+	public static void clickLinks(String link1) {
+		getDriver().findElement(By.xpath(commonLink.replace("THANU", link1))).click();
+		LOGGER.info("Link " + link1 + " Clicked");
 		staticWait(3);
 	}
 }
